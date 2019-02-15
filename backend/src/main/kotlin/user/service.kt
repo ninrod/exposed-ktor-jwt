29885a66b6199ea891cb9d0a5877ac97c0dc5963
Filipe Correa Lima da Silva <filipe.silva@bcb.gov.br>
2019-02-15 19:57:37 -0200

    remove logs

--- backend/src/main/kotlin/user/service.kt
+++ backend/src/main/kotlin/user/service.kt
@@ -6,18 +6,11 @@ import org.jetbrains.exposed.sql.transactions.transaction
 
 fun Long.par() = this % 2 == 0L
 
-fun authGetUsers(token: JWTPrincipal): List<UserDTO> {
-    println("authGetUsers()")
-    return getUsers().filter {
+fun authGetUsers(token: JWTPrincipal): List<UserDTO> = getUsers().filter {
         if (token.payload.getClaim("id").asLong().par()) it.id.value.par() else !it.id.value.par()
     }.map { it.toDTO() }
-}
 
-
-fun getUsers(): List<User> = transaction {
-    println("getUsers()")
-    User.all().toList()
-}
+fun getUsers(): List<User> = transaction { User.all().toList() }
 
 fun findUserByCredentials(credential: UserPasswordCredential): User? =
         getUsers().filter { it.password == credential.password && it.login == credential.name }.firstOrNull()

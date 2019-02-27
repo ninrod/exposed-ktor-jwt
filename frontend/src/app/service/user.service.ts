f35d3c7795a098a26c1816789b45e93a274fd1ae
Filipe Correa Lima da Silva <filipe.silva@bcb.gov.br>
2019-02-27 18:29:22 -0300

    tweak

--- frontend/src/app/service/user.service.ts
+++ frontend/src/app/service/user.service.ts
@@ -1,12 +1,13 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { User } from '../model/user';
+import { HttpClient } from '@angular/common/http'
+import { Injectable } from '@angular/core'
+import { Observable } from 'rxjs'
+import { User } from '../model/user'
 
 @Injectable()
 export class UsersService {
   constructor(private http: HttpClient) { }
+
   public getUsers(): Observable<User[]> {
-    return this.http.get<User[]>('api/secret');
+    return this.http.get<User[]>('api/secret')
   }
 }

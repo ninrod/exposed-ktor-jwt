3aff0c76329e870f5c395a2d8af6e6bd5bd28361
Filipe Correa Lima da Silva <filipe.silva@bcb.gov.br>
2019-02-27 18:30:46 -0300

    tweak

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { User } from 'src/app/model/user';
-import { UsersService } from 'src/app/service/user.service';
+import { Component, OnInit } from '@angular/core'
+import { User } from 'src/app/model/user'
+import { UsersService } from 'src/app/service/user.service'
 
 @Component({
   selector: 'app-home',
